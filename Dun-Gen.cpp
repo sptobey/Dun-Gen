43cf9b3136e08d94aacc2c3a8d21dbec50a00324
Samuel Tobey <Samuel.P.Tobey@Colordao.edu>
2014-10-27 21:35:34 -0600

    added constructor

--- Dun-Gen.cpp
+++ Dun-Gen.cpp
@@ -6,22 +6,32 @@
 #define FLOOR 0
 #define WALL 1
 
-/* Dungeon structure */
-typedef struct {
-	int seed;
-	int height, width;
-} dungeon;
+/* Dungeon class */
+class Dungeon {
+private:
+public:
+  int seed;
+  int height, width;
+  Dungeon(int, int, int);
+};
+// Constructor
+Dungeon::Dungeon(int s, int w, int h) {
+  seed = s;
+  width = w;
+  height = h;
+}
+/* End Dungeon class */
 
 int main() {
-	
-	std::ofstream ofs("test.txt");
-	
-	if(!ofs) {
-		std::cerr<<"Failed to open output file"<<std::endl;
-	}
-	
-	ofs<<"\nThis is a test.\nRandom text here...\n\n";
-	ofs.close();
-	
-	return 0;
+  
+  std::ofstream ofs("test.txt");
+  
+  if(!ofs) {
+    std::cerr<<"Failed to open output file"<<std::endl;
+  }
+  
+  ofs<<"\nThis is a test.\nRandom text here...\n\n";
+  ofs.close();
+  
+  return 0;
 }
