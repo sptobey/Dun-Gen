0247d8f144396bd2ea51d9e49fa22f3e7c05e06c
David Bittle <user@cu-cs-vm.(none)>
2014-11-20 00:48:42 -0700

    doxygen comments, some differnt code for pathfind

--- Dun-Gen.cpp
+++ Dun-Gen.cpp
@@ -1,8 +1,9 @@
-/* Dun-Gen.cpp
- * Christopher Audette,
- * Samuel P. Tobey,
- * David Bittle,
- * Philip Rowe
+/*! Dun-Gen.cpp
+ * @author Christopher Audette,
+ * @author Samuel P. Tobey,
+ * @author David Bittle,
+ * @author Philip Rowe
+ * @brief A project that builds a 'dungeon' that has well and rooms and paths generated between them. The number of rooms is random and the size can be passed in
  */
 #include <fstream>
 #include <iostream>
@@ -13,10 +14,15 @@
 #include <sstream>
 #include "Subdungeon.h"
 #include <istream>
+#include <list>
+#include <typeinfo>
+
 
 #define BLANK 0
 #define FLOOR 1
 #define WALL 2
+#define PATH 8
+#define DOOR 6
 
 using namespace std;
 
@@ -37,6 +43,7 @@ public:
   int height, width;
   void outputDungeon(std::string name);
   void printDungeon(string fileName);
+  list<Subdungeon> roomList;
 };
 // Full Constructor
 Dungeon::Dungeon (int w, int h, unsigned long int s) {
@@ -71,6 +78,12 @@ Dungeon::Dungeon(){
   buildDungeon(9, 225, 100);
 }
 
+/*!
+ * 
+ * @brief puts the dungeon into a file
+ * @param dungeon_name the name of the dungeon that was passed in. It is made as a .txt file
+ * */
+
 void Dungeon::outputDungeon(string dungeon_name){
   dungeon_name.append(".txt");
   const char* c = dungeon_name.c_str();
@@ -96,8 +109,6 @@ void Dungeon::outputDungeon(string dungeon_name){
   ofs.close();
 }
 
-
-
 // Builds a dungeon full of blank tiles
 void Dungeon::buildEmpty(){
   dCont = new short unsigned int*[height];  //hooray! Pointers.  Note that this isn't actually deleted anywhere yet.
@@ -111,6 +122,13 @@ void Dungeon::buildEmpty(){
   }
 }
 
+
+
+/*!
+ * @brief prints the dungeon file
+ * @param dungeon_name the name of the dungeon that was passed in. It is made as a .txt file
+ * */
+ 
 void Dungeon::printDungeon(string fileName){
   ifstream dungeonOutput;
   fileName.append(".txt"); 
@@ -120,15 +138,24 @@ void Dungeon::printDungeon(string fileName){
   
 
 
+  
+/*!
+ * @brief builds the main dungeon
+ * @param rmin minimum room area
+ * @param rmax max room area
+ * @param rnum max number of rooms
+ * */
+ 
+
 // Builds the actual full dungeon from one full of blank tiles.
 void Dungeon::buildDungeon(unsigned short rmin, unsigned short rmax, unsigned short rnum){
   // Using the given seed guarantees it will generate the same dungeon every for the same seed
   srand(Dungeon::seed);
   unsigned short roomMax, roomMin, roomNumMax;
   Subdungeon **rooms;
-  roomMax = rmax;         // Maximum room area
-  roomMin = rmin;         // Minimum room area
-  roomNumMax = rnum;      // Maximum number of rooms possible
+  roomMax = rmax;        
+  roomMin = rmin;         
+  roomNumMax = rnum;      
   rooms = new Subdungeon *[roomNumMax];
   
   // Generating list of rooms
@@ -155,6 +182,7 @@ void Dungeon::buildDungeon(unsigned short rmin, unsigned short rmax, unsigned sh
     rooms[r] = new Subdungeon(0, posx, posy, roomHeight, roomWidth);
   }
   
+  
   // Placing rooms; will just re-key and place a different room if a room fails a test.
   for (int k = 0; k < roomNumMax; ++k){
     bool roomOverlap = false;
@@ -170,7 +198,9 @@ void Dungeon::buildDungeon(unsigned short rmin, unsigned short rmax, unsigned sh
     for (int i = posy; i <= posyEnd; ++i){
       for (int j = posx; j <= posxEnd; ++j){
         // Check if each space is blank, if not, then room overlap
-        if (dCont[i][j] != BLANK){
+        
+        //Added checks so that there are spaces between rooms
+        if (dCont[i][j] != BLANK or dCont[i+1][j] or dCont[i-1][j] != BLANK or dCont[i][j+1] != BLANK or dCont[i][j-1] != BLANK){
           roomOverlap = true;
           //set i, j to break out of loop
           j = posxEnd;
@@ -196,8 +226,112 @@ void Dungeon::buildDungeon(unsigned short rmin, unsigned short rmax, unsigned sh
         dCont[posy   ][j] = WALL;
         dCont[posyEnd][j] = WALL;
       }
+   
+   //adds room to a list of rooms
+     roomList.push_back(* rooms[k]);
+   
+   //   cout << "a room has been made with a central point of " << posxEnd-posx << "," << posyEnd-posy << endl;
+    
     }
+    
+    
+    
   }
+  
+ 
+		Subdungeon oldDungeon = *roomList.begin(); 
+//  Subdungeon startRoom;
+//makes a "door" on the left hand side starts to find a way around
+  for (list<Subdungeon>::iterator iterator = roomList.begin(), end = roomList.end(); iterator != end; ++iterator) {
+  //  if(startRoom != NULL)
+		int doory = iterator->boundsBot[0]+(iterator->boundsBot[1]-iterator->boundsBot[0])/2;
+		//int midx = iterator->
+		int doorx = iterator->boundsTop[0];
+		dCont[doory][doorx]= DOOR;
+		dCont[doory][doorx-1]= PATH;
+		doorx = doorx-1;
+		//if(oldDungeon
+		
+		if (iterator != roomList.begin()){
+			
+			//Subdungeon oldDungeon = roomList.back();
+			cout << oldDungeon.boundsBot[0]  <<endl;
+			
+			int destinationDoory = oldDungeon.boundsBot[0]+(oldDungeon.boundsBot[1]-oldDungeon.boundsBot[0])/2;
+			int destinationDoorx = oldDungeon.boundsTop[0];
+		//	cout << "the destination is: "<< destinationDoory << destinationDoorx << endl;
+			
+			
+	//		cout << "the doors location is " << doorx << endl;
+	//		cout << "the destinations is " << destinationDoorx << endl;	
+			
+
+			//basic path finding stuff		
+			//while(doory!=destinationDoory && doorx != destinationDoorx-1){
+				
+			int i = 0;
+			while((doory!=destinationDoory || doorx != destinationDoorx) && i<100){
+				
+				if(doory > destinationDoory && dCont[doory-1][doorx]!=WALL && dCont[doory-1][doorx]!=DOOR){
+						dCont[doory-1][doorx]= PATH;
+						doory = doory -1;
+	
+					}
+				
+				if(doory < destinationDoory && dCont[doory+1][doorx]!=WALL && dCont[doory+1][doorx]!=DOOR){
+						dCont[doory+1][doorx]= PATH;
+						doory = doory +1;
+					}
+			
+			// && dCont[doory][doorx-1]!=WALL
+				if(doorx < destinationDoorx  && dCont[doory][doorx+1]!=WALL && dCont[doory][doorx+1]!=DOOR){
+							dCont[doory][doorx+1]= PATH;
+							doorx = doorx +1;
+					}
+				if(doorx > destinationDoorx && dCont[doory][doorx-1]!=WALL  && dCont[doory][doorx-1]!=DOOR){
+	
+						dCont[doory][doorx-1]= PATH;
+						doorx = doorx -1;
+					}
+			i++;
+			//if(i%10==0){cout<<destinationDoorx << destinationDoory<< endl;}
+			
+			oldDungeon = *iterator;
+			}
+			
+			
+			//while(doory > destinationDoory && dCont[doory][doorx-1]!=WALL){
+						//dCont[doory][doorx-1]= PATH;
+						//doory = doory -1;
+	
+					//}
+					//doory++;
+				//// && dCont[doory][doorx-1]!=WALL
+				//while(doorx-1 < destinationDoorx-1  && dCont[doory][doorx-1]!=WALL){
+	
+						//dCont[doory][doorx-1]= PATH;
+						//doorx = doorx +1;
+					//}
+				//while(doorx-1 > destinationDoorx-1 && dCont[doory][doorx-1]!=WALL){
+	
+						//dCont[doory][doorx-1]= PATH;
+						//doorx = doorx -1;
+					//}
+				
+				//while(doory < destinationDoory && dCont[doory][doorx-1]!=WALL){
+						//dCont[doory][doorx-1]= PATH;
+						//doory = doory +1;
+					//}
+			
+			
+			
+			
+			}
+	//	}
+		//cout<<oldDungeon.boundsTop[0]<<endl;
+		
+}
+	
 
 // Place walls on the edges of the dungeon
   for(int i = 0; i < height; i++){
@@ -208,6 +342,8 @@ void Dungeon::buildDungeon(unsigned short rmin, unsigned short rmax, unsigned sh
     dCont[0][j] = WALL;
     dCont[height-1][j] = WALL;
   }
+  
+  
   
   
   
